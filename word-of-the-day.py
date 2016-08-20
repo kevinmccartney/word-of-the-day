5c0583065e748b6a134bd4db3a64f18b1cdde55b
Kevin McCartney <hello@kevinmccartney.is>
2016-08-19 20:41:23 -0400

    split everything up into functions

--- word-of-the-day.py
+++ word-of-the-day.py
@@ -4,49 +4,83 @@ from TwilioCredentials import *
 import requests
 import datetime
 from bs4 import BeautifulSoup
+from bs4 import UnicodeDammit
 from twilio.rest import TwilioRestClient
+import pprint
+import textwrap
 
-# getting today's date & formatting
-today = datetime.date.today()
-datestring = today.strftime('%Y/%m/%d')
+def buildURL():
+    global site
+ 
+    # getting today's date & formatting
+    today = datetime.date.today()
+    datestring = today.strftime('%Y/%m/%d')
 
-# building the url & making the request
-url = "http://learning.blogs.nytimes.com/{0}/word-of-the-day-quiz".format(datestring)
-site = requests.get(url)
+    # building the url & making the request
+    url = "http://learning.blogs.nytimes.com/{0}/word-of-the-day-quiz".format(datestring)
+    site = requests.get(url)
+   
+    return site
 
-# loading the page into BeautifulSoup
-page = site.text
-soup = BeautifulSoup(page, 'html.parser')
+def scrape():
+    global msg_raw
 
-# finding the word of the day container
-wod_container = soup.find("article", {"class": "category-word-of-the-day"})
+    # loading the page into BeautifulSoup
+    global site
+    page = site.text
+    soup = BeautifulSoup(page, 'html.parser')
 
-# pulling out the elements we need
-# TODO: test that the wod_def & wod_example to make sure they behave consistently 
-wod_word = wod_container.find('h3', {"class": "wod"})
-wod_def = wod_container.find_all('p', {"class": "story-body-text"}, limit=2)
-wod_example = wod_container.find('blockquote')
+    # finding the word of the day container
+    wod_container = soup.find("article", {"class": "category-word-of-the-day"}).find("div", {"class" : "entry-content"})
 
-# making the elements pretty
-pretty_wod_word = wod_word.get_text().encode('utf-8')
-pretty_wod_def = '\n\n'.join(i.get_text() for i in wod_def).encode('utf-8')
-pretty_wod_example = wod_example.get_text().encode('utf-8')
+    # pulling out the elements we need
+    # TODO: test that the wod_def & wod_example to make sure they behave consistently 
+    wod_word =  wod_container.find('h3', {"class": "wod"})
+    wod_def = wod_container.find_all('p', {"class": "story-body-text"}, limit=2)[0]
+    wod_example_intro = wod_container.find_all("p", {"class": "story-body-text"}, limit=2)[1]
+    wod_example_text = wod_container.find('blockquote')
 
-args = (pretty_wod_word, pretty_wod_def, pretty_wod_example)
+    msg_raw = {
+        'word': wod_word, 
+        'def' : wod_def, 
+        'example_intro': wod_example_intro, 
+        'example_text': wod_example_text
+    }
 
-msg = '''
-===========
-WORD OF THE DAY
-==========
+    return msg_raw
+    
+def cleanMsg():
+    global msg_raw    
 
-{0}
------
-{1}
+    for key, value in msg_raw.iteritems():
+        msg_raw[key] = value.get_text().encode('utf-8')
 
-{2}'''.format(*args)
+    args = (msg_raw["word"], msg_raw["def"], msg_raw["example_intro"], msg_raw["example_text"])
+    msg = textwrap.dedent(
+    '''
+    ===========
+    WORD OF THE DAY
+    ==========
 
-client = TwilioRestClient(account_sid, auth_token)
+    {0}
+    {1}
+    -----
+    {2}
+    {3}
+    '''.format(*args)
+    )
 
-message = client.messages.create(body=msg,
-    to= twilio_to,
-    from_= twilio_from)
+def sendSMS():
+    client = TwilioRestClient(account_sid, auth_token)
+
+    message = client.messages.create(body=msg,
+        to= twilio_to,
+        from_= twilio_from)
+
+def main():
+    buildURL()
+    scrape()
+    cleanMsg()
+    sendSMS()
+
+main()
